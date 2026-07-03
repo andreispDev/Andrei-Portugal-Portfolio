e7883a69305cef719c977c1cf81c73b9f275b9cb
unknown <andreispdev@gmail.com>
2026-07-03 14:12:37 +0800

    removed unused import

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,4 @@
-import React, { useEffect } from "react";
+import { useEffect } from "react";
 import { styles } from "./style";
 import Background from "./components/Background";
 import Navbar from "./components/Navbar";
